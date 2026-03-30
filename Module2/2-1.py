9b04e4de913b1b7fae30ea3cd035de41fb3dd40b
Iris <emilyhuang12380@gmail.com>
2026-03-30 13:49:51 +0800

    first

--- Module2/2-1.py
+++ Module2/2-1.py
@@ -1,10 +1,30 @@
 # 建立第一支 Python 程式
 
 # 宣告變數
-
-
-
+name = "Iris"
 
 # 輸出變數
+print(name)
 
+#cd:切換資料夾
+# ../:回到上一層
+
+'''
+VS code快捷鍵
+ctrl +  ` 快速跳到終端機
+ctrl + 1 回到第一個 Page
+ctrl + W 關掉page
+ctrl + B 側邊攔
+ctrl + P 打開檔案
+ctrl + Tab 切換分頁
+
+當機處理:Ctrl + Shift + P -> 輸入Reload Window
+'''
+
+'''
+寫程式用的快捷鍵: 
+複製程式碼 Alt + Shift + ↓ / ↑
+移動程式碼 Alt + ↓ / ↑
+一次修改多個變數 Ctrl + D 改完之後 Esc 取消選取
+'''
 
--- Module3/practice.py
+++ Module3/practice.py
@@ -5,6 +5,22 @@
 # 數字太小 要提示「請輸入更大的數字」
 # 使用者猜對要回傳「恭喜中獎」
 
+password = 34
+
+while True:
+    data = input("請輸入1-100的數字")
+    data = int(data) 
+    if data > 100 or data < 1:
+        print("超出範圍請重新輸入")
+    elif data > password:
+        print("請輸入更小的數字")
+    elif data < password:
+        print("請輸入更大的數字")
+    else: 
+        print("恭喜中獎")
+        break
+        
+
 
 
 
